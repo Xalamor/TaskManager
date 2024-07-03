81b64edf7148be06d87d54639538535dcee5a761
Xalamor <kornevaleks0705.gmail.com>
2024-07-03 11:28:24 +0300

    Папка с проектом джанго для менеджера задач с авторизацией

--- TaskManager/main/forms.py
+++ TaskManager/main/forms.py
@@ -24,3 +24,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите имя'
+            }),
+            'password': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите пароль'
+            })}
